68d093abd878c6e6491075a36087eb56549e8396
LucasBargas <lucas.bargasoficial@gmail.com>
2023-01-05 22:38:00 -0300

    Login and register pages - protected

--- src/pages/cadastre-se/index.js
+++ src/pages/cadastre-se/index.js
@@ -3,6 +3,25 @@ import React from 'react';
 import RegisterPage from '../../templates/RegisterPage';
 import bannerImg from '../../../public/photographer_register.jpg';
 import HeadPerPage from '../../components/HeadPerPage';
+import { parseCookies } from 'nookies';
+
+export const getServerSideProps = async (context) => {
+  const cookies = parseCookies(context);
+  const token = cookies.galleryPhotoApiToken;
+
+  if (token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 const Register = () => {
   const router = useRouter();
--- src/pages/entrar/index.js
+++ src/pages/entrar/index.js
@@ -3,6 +3,25 @@ import React from 'react';
 import LoginPage from '../../templates/LoginPage';
 import HeadPerPage from '../../components/HeadPerPage';
 import bannerImg from '../../../public/photographer_login.jpg';
+import { parseCookies } from 'nookies';
+
+export const getServerSideProps = async (context) => {
+  const cookies = parseCookies(context);
+  const token = cookies.galleryPhotoApiToken;
+
+  if (token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 const Login = () => {
   const router = useRouter();
